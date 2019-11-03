8862212491a25404cf760ace8c7a2142b9626c49
Fer <yilmazferhat1986@gmail.com>
2019-11-03 11:07:52 -0500

    Feature : Ran 2nd test for subtraction

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,7 +1,12 @@
+from Calculator.Subtraction import subtraction
 class Calculator:
     def __init__(self):
         pass
 
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
 
 
 
